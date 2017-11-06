c118df7ad1c6d29feb3f2a430999ea9d101f5b2c
Michal <mggrupa@gmail.com>
2017-11-06 15:36:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,7 @@ function happyHolidaysTo(name) {
 }
 
 function happyHolidayTo(holiday, name) {
-  return "Happy Independence Day, you"
+var holiday = "Independence Day"
+var name = "you"
+  return 'Happy ${holiday}, ${name}!'
 }
